dcdcace286efbcccb0c97a689ff473db66872e5d
Maycon Negrini <maycon128@hotmail.com>
2023-03-27 22:46:07 -0300

    alterations to work in the new pokedex (vue3)

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-// const personRoutes = require('./routes/personRoutes');
+
 const typeRoutes = require('./routes/typeRoutes');
 const pokemonRoutes = require('./routes/pokemonRoutes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use('/', personRoutes);
 app.use('/', typeRoutes);
 app.use('/', pokemonRoutes);
 
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
+const DB_USER = process.env.DB_USER; //Admin
+const DB_PASS = process.env.DB_PASS; //admin
 
 mongoose
   .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster-api.qt8ysip.mongodb.net/database?retryWrites=true&w=majority`)
-  .then(() => { console.log('Database successfully connected'); app.listen(3000); })
+  .then(() => { console.log('Database successfully connected'); app.listen(4000); })
   .catch((err) => console.log(err))
