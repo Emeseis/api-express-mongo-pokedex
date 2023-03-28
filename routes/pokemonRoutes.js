88b876918081a21e10d1563210a1c4101a016698
Maycon Negrini <maycon128@hotmail.com>
2023-03-27 22:46:07 -0300

    alterations to work in the new pokedex (vue3)

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -3,7 +3,7 @@ const Pokemon = require('../models/Pokemon');
 
 router.post('/pokemon', async (req, res) => {
   const { id, entry, name, sprite, types, gen } = req.body;
-  const pokemon = { id, entry, name, sprite, types, gen }
+  const pokemon = { id, entry, name, sprite, types, gen };
 
   try {
     await Pokemon.create(pokemon);
@@ -13,33 +13,67 @@ router.post('/pokemon', async (req, res) => {
   }
 });
 
-router.get('/pokedex', async (req, res) => {
+router.post('/pokemons', async (req, res) => { 
+  let query = {}; 
+
+  const { name, order, gen, type, from, to } = req.body;
+
+  if (gen != 'All') query.gen = gen;
+  if (type != 'All') query.types = { $elemMatch: { "type.name": type  } };
+  if (name) query.name = { $regex: new RegExp(name.trim(), "i") };
+
+  // if (from && to)  query.id = { $gte: from, $lte: to };  // filtros não estão mais sendo usados
+  // if (from && !to) query.id = { $gte: from };
+  // if (!from && to) query.id = { $lte: to };
+
   try {
-    const pokemon = await Pokemon.find();
-    res.status(200).json(pokemon);
+    const pokemons = await Pokemon.find(query).sort({ id: order });
+    res.status(200).json({ length: pokemons.length, pokemons });
   } catch (err) {
-    res.status(500).json({ error: err })
+    res.status(500).json({ error: err });
   }
 });
 
 router.get('/pokedex/:gen', async (req, res) => {
   const gen = req.params.gen;
+
   try {
     const pokemon = await Pokemon.find({ gen });
     res.status(200).json(pokemon);
   } catch (err) {
-    res.status(500).json({ error: err })
+    res.status(500).json({ error: err });
   }
 });
 
 router.get('/pokemon/:id', async (req, res) => {
   const id = req.params.id;
+  
   try {
     const pokemon = await Pokemon.findOne({ id });
     if (!pokemon) { res.status(422).json({ msg: 'Pokemon not found' }); return; }
     res.status(200).json(pokemon);
   } catch (err) {
-    res.status(500).json({ error: err })
+    res.status(500).json({ error: err });
+  }
+});
+
+router.put('/pokemon', async (req, res) => {
+  const { id, entry } = req.body;
+  try {
+    await Pokemon.updateOne({ id }, { entry });
+    res.status(201).json({ msg: 'Pokemon successfully updated' });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+router.delete('/pokemon/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Pokemon.deleteOne({ id });
+    res.status(201).json({ msg: 'Pokemon successfully deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err });
   }
 });
 
